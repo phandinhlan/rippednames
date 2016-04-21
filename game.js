6444369a9a48ec92655dec8e5e7eddcbc87f775d
p.lan <pdl.dev@outlook.com>
2016-04-21 16:46:19 -0700

    Ground work for game start

--- game.js
+++ game.js
@@ -11,12 +11,18 @@ Team.prototype.Team = function (Id) {
 }
 
 //--    Game
-Game.prototype.Game = function (creator) {
+Game.prototype.Game = function (Id, creator) {
     Hoek.assert(this instanceof Game, 'Game must be instantiated using new');
+    this.Id = Id;
     this.players = new Map();
     this.players.set(creator.Id, creator);
     this.teams = [ new Team(0), new Team(1) ];  //note hardcoded to 2 teams
     this.teamedUp = false;
+    //count down to 0 when all conditions to start game is meet
+    //1 count for spymasters chosen, which requires players to have teamed up already
+    const READY_TO_START_CONDITION_MAX = 1;
+    this.readyToStart = READY_TO_START_CONDITION_MAX;
+    this.gameStarted = false;
 }
 
 //--    Game setup
@@ -45,4 +51,33 @@ Game.prototype.ChooseSpyMasters = function () {
     
     randomIndex = Math.floor(Math.random() * this.teams[1].size);
     this.teams[1].spyMaster = this.players[randomIndex];
+    
+    this._CreditReadyToStartCondition();
+}
+
+Game.prototype.Start = function () {
+    if (false == this._IsReadyToStart()) {
+        return;
+    }
+    
+    //TODO: implement
+    //Setup board
+    //Select spymaster map
+    //What else?
+    
+    this.gameStarted = true;
+}
+
+Game.prototype.IsReadyToStart = function () {
+    return (0 == this.readyToStart ? true : false );
+}
+
+Game.prototype._CreditReadyToStartCondition = function () {
+    Hoek.assert(false == this.IsReadyToStart(), 'Ready to start condition out of sync');
+    --this.readyToStart;
+}
+
+Game.prototype._DebitReadyToStartCondition = function () {
+    Hoek.assert(READY_TO_START_CONDITION_MAX != this.readyToStart, 'Ready to start condition out of sync');
+    ++this.readyToStart;
 }
