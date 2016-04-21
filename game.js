c1b1bf9e83194168157ad328073a9054457ab82d
p.lan <pdl.dev@outlook.com>
2016-04-21 14:07:40 -0700

    Obliged to convention

--- game.js
+++ game.js
@@ -1,45 +1,48 @@
-function Player(Id) {
+Player.prototype.Player = function (Id) {
+    Hoek.assert(this instanceof Player, 'Player must be instantiated using new');
     this.Id = Id;
 }
 
-function Team(Id) {
+Team.prototype.Team = function (Id) {
+    Hoek.assert(this instanceof Team, 'Team must be instantiated using new');
     this.Id = Id;
     this.players = new Map();
     this.spyMaster = null;
 }
 
-function Game(creator) {
+//--    Game
+Game.prototype.Game = function (creator) {
+    Hoek.assert(this instanceof Game, 'Game must be instantiated using new');
     this.players = new Map();
     this.players.set(creator.Id, creator);
     this.teams = [ new Team(0), new Team(1) ];  //note hardcoded to 2 teams
     this.teamedUp = false;
-    
-    //--    Setup
-    this.AddPlayer = function (player) {
-        this.players.set(player.Id, player);
+}
+
+//--    Game setup
+Game.prototype.AddPlayer = function (player) {
+    this.players.set(player.Id, player);
+}
+
+Game.prototype.AssignPlayerToTeam = function (playerId, teamId) {
+    var otherTeamId = (0 == teamId) ? 1 : 0;    //note: hardcoded to 2 teams
+    if (true == this.teams[otherTeamId].has(playerId)) {
+        this.teams[otherTeamId].delete(player.Id);        
+    }
+    if (false == this.teams[teamId].has(player.Id)) {
+        this.teams[teamId].set();    
+    }
+}
+
+Game.prototype.ChooseSpyMasters = function () {
+    if (false == this.teamedUp) {
+        //TODO: handle\report error
+        return;
     }
     
-    this.AssignPlayerToTeam = function (player, teamId) {
-        var otherTeamId = (0 == teamId) ? 1 : 0;    //note: hardcoded to 2 teams
-        if (true == this.teams[otherTeamId].has(teamId)) {
-            this.teams[otherTeamId].delete(player.Id);        
-        }
-        if (false == this.teams[teamId].has(player.Id)) {
-            this.teams[teamId].set();    
-        }   
-    }
+    var randomIndex = Math.floor(Math.random() * this.teams[0].size);
+    this.teams[0].spyMaster = this.players[randomIndex];
     
-    this.ChooseSpyMasters = function () {
-        if (false == this.teamedUp) {
-            //TODO: handle\report error
-            return;
-        }
-        
-        var randomIndex = Math.floor(Math.random() * this.teams[0].size);
-        this.teams[0].spyMaster = this.players[randomIndex];
-        
-        randomIndex = Math.floor(Math.random() * this.teams[1].size);
-        this.teams[0].spyMaster = this.players[randomIndex];
-    }
-    //--
+    randomIndex = Math.floor(Math.random() * this.teams[1].size);
+    this.teams[1].spyMaster = this.players[randomIndex];
 }
